22805b797bcfbac87fdea1ac0e7255e13af05334
jchamois <jchamois@clever-age.com>
2025-07-23 11:51:33 +0200

    moved to dart sass

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,29 @@
-import toto from './other.js';
+console.log('fdsfsdfds')
 
-toto();
 
-console.log('js launched !')
+const box = document.querySelector('.box');
+const stop = document.querySelector('.stop');
+
+let left = 0;
+let transX = 0;
+let dir = 1
+let raf;
+
+const backandforth = () => {
+
+    if(transX > 500){
+        dir = -1;
+    } else if( transX == 0){
+        dir = 1
+    }
+
+    transX += 5 * dir;
+    box.style.transform = `translateX(${transX}px)`;
+    raf = requestAnimationFrame(backandforth);
+}
+
+raf = requestAnimationFrame(backandforth);
+
+stop.addEventListener('click',e => {
+    cancelAnimationFrame(raf)
+})
